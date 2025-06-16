d0081b98043e866d3958f8a8252b4b7c8e8877ac
rxkko <lopushko256@gmail.com>
2025-06-16 15:05:19 +0300

    Added password hashing

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -14,5 +14,4 @@ def get_product_service(
     return ProductService(product_repo)
 
 def get_user_by_role():
-    # Placeholder for user role dependency
     pass
--- src/schemas/user.py
+++ src/schemas/user.py
@@ -2,7 +2,12 @@ from datetime import datetime
 from pydantic import BaseModel, EmailStr
 
 
-class UserBase(BaseModel):
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class UserCreate(BaseModel):
     name: str
     username: str
     email: EmailStr
@@ -10,12 +15,19 @@ class UserBase(BaseModel):
     is_admin: bool = False
 
 
-class UserCreate(UserBase):
-    password: str  # Исходный пароль для хэширования
+class UserUpdate(BaseModel):
+    name: str | None = None
+    username: str | None = None
+    password: str | None = None
+    email: EmailStr | None = None
+    is_active: bool | None = None
+    is_admin: bool | None = None
 
 
-class UserRead(UserBase):
-    id: int
+class UserRead(BaseModel):
+    name: str
+    username: str
+    email: EmailStr
     created_at: datetime
 
     class Config:
