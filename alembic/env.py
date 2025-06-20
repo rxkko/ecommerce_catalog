b93f6274493109217135da9c83af716aa0f4d15f
rxkko <lopushko256@gmail.com>
2025-06-20 15:13:31 +0300

    set up PostgreSQL connection with Docker

--- alembic/env.py
+++ alembic/env.py
@@ -22,7 +22,7 @@ if DATABASE_URL is None:
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
-
+    
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
--- src/core/config.py
+++ src/core/config.py
@@ -1,28 +1,30 @@
 from pydantic_settings import BaseSettings
 
-
 class Settings(BaseSettings):
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
-    REFRESH_TOKEN_EXPIRE_DAYS: int
+    REFRESH_TOKEN_EXPIRE_DAYS: int 
+
+    REDIS_HOST: str
+    REDIS_PORT: int
 
     @property
     def DATABASE_URL(self):
-        return f"postgresql+psycopg2://postgres:postgres@postgres:5432/app_db"
+        return f"postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
 
     @property
     def DATABASE_URL_asyncpg(self) -> str:
-        return f'postgresql+asyncpg://postgres:postgres@postgres:5432/app_db'
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
-
 
 settings = Settings()
